8ec4e323c1c531be055450376704c70a7e18ca90
lathiruchika <lathiruchika@gmail.com>
2026-08-18 15:28:20 +0530

    Create tests/epam-client-work.spec.ts

---
+++ tests/epam-client-work.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('EPAM website navigation', () => {
+  test('navigates from Services to Client Work', async ({ page }) => {
+    // steps will be added here
+  });
+});
